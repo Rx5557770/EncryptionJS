407a791d4dae3b52327a06b82506c7104c6449ea
Rx5557770 <69506638+Rx5557770@users.noreply.github.com>
2024-10-10 23:35:35 +0800

    Create 超星.js

---
+++ 超星.js
@@ -0,0 +1,20 @@
+function encryptByAES(message, key) {
+    let CBCOptions = {
+        iv: CryptoJS.enc.Utf8.parse(key),
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    };
+    let aeskey = CryptoJS.enc.Utf8.parse(key);
+    let secretData = CryptoJS.enc.Utf8.parse(message);
+    let encrypted = CryptoJS.AES.encrypt(
+        secretData,
+        aeskey,
+        CBCOptions
+    );
+    return CryptoJS.enc.Base64.stringify(encrypted.ciphertext);
+}
+
+
+let transferKey = "u2oh6Vu^HWe4_AES";
+password = encryptByAES("11012013014", transferKey);
+uname = encryptByAES("11012013014", transferKey);
